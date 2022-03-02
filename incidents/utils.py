d681993e125d35ddbc509149bc6969afd673b599
magsletscode.python <60409904+Maghanga97@users.noreply.github.com>
2022-03-02 02:03:07 -0800

    Update utils.py

--- incidents/utils.py
+++ incidents/utils.py
@@ -18,11 +18,11 @@ def render_to_pdf(template_src, context_dict={}):
 def not_null(param):
     return param != "" and param != None
 
-def sendsms(phone=None, ref):
+def sendsms(phone, ref):
     url = "https://api.mobitechtechnologies.com/sms/sendsms"
     headers = CaseInsensitiveDict()
     headers["h_api_key"] = "123b08dcc32921eaf7c87d15e4fda2c31f808fa5ab6709d8e7fc3878b34593a7"
     headers["Content-Type"] = "application/json"
-    data = '{"mobile" : phone,"response_type": "json","sender_name":"23107","service_id" : 0,"message" : f"your reference number is: +{ref}"}'
+    data = '{"mobile" : {phone},"response_type": "json","sender_name":"23107","service_id" : 0,"message" : f"your reference number is: +{ref}"}'
     resp = requests.post(url, headers=headers, data=data)
     return(resp.status_code)
