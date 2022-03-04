1993f3d024197591457ce3cdad98d18a69316ca2
magsletscode.python <60409904+Maghanga97@users.noreply.github.com>
2022-03-04 05:37:41 -0800

    Update urls.py

--- cpccs/urls.py
+++ cpccs/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from django.views.static import serve
 from django.conf.urls import url
 from django.conf import settings
-from incidents.views import  incident_pdf, overall_report, department_incidents_types, incidents_view, incidents_types, incidents_list, department_incidents, registration_view, add_department, manage_users, admin_view, details, edit_user_view, main_admin_panel, login
+from incidents.views import  incident_pdf, overall_report, search_incident, search_results, department_incidents_types, incidents_view, incidents_types, incidents_list, department_incidents, registration_view, add_department, manage_users, admin_view, details, edit_user_view, main_admin_panel, login
+                        
 from incidents.db_insert import change_user, insert_function, registration_back_end, add_department_back_end, assign_user_incident, change_user
 
 urlpatterns = [
@@ -18,7 +19,7 @@ urlpatterns = [
     path('assign/', assign_user_incident),
     path('pdf/<reference>/', incident_pdf),
     path('report/', overall_report),
-    path('view_user/<clicked_from>/<admin>/<user_id>/', edit_user_view),
+    path('view_user/<clicked_from>/<admin>/<user_id>/', edit_user_view),    
     path('edit/', change_user),
     path('login/', login),
     path('main-dashboard/<user>/<session>/', main_admin_panel),
@@ -27,6 +28,8 @@ urlpatterns = [
     path('department-incidents/<clicked_from>/<get_user>/<incident_status>/', department_incidents),
     path('incident-type/<get_user>/<incident_type>/<clicked_from>/', incidents_types),
     path('department-incident-type/<get_user>/<incident_type>/<clicked_from>/', department_incidents_types),
+    path('search/<clicked_from>/<user_id>/', search_incident),
+    path('search-results/', search_results),
 
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
